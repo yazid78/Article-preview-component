2891ca1089e8f4b79084a3e44f6b2c19678d1bdb
Yazid <yazid780@outlook.fr>
2024-08-26 00:09:19 +0200

    end coding

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let a = 2;
-a = 5;
-console.log(a);
+const barShare = document.querySelector(".share");
+const iconeShare = document.querySelector(".shareIcone");
+
+iconeShare.addEventListener("click", (e) => {
+  e.stopPropagation();
+  barShare.style.visibility = "visible";
+});
+document.querySelector("body").addEventListener("click", () => {
+  barShare.style.visibility = "hidden";
+});
